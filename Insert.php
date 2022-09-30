3669761b49c6638f6bf9e6a75a9aabbd4ef0fd37
JasonZelmanovich <114768950+JasonZelmanovich@users.noreply.github.com>
2022-09-30 14:06:47 -0400

    Add files via upload

---
+++ Insert.php
@@ -0,0 +1,47 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+$host = "localhost";
+$username = "Jason";
+$password = "Jason1253!";
+$db = "HBS";
+$secretKey = "6LfMeZwhAAAAAGMAjdESqA72sPDTE410_9LsQNjn";
+
+
+
+$conn = mysqli_connect($host, $username, $password, $db) or die("cannot connect");
+
+$name = $_POST["modal-name"];
+$email = $_POST["modal-email"];
+$message = $_POST["modal-message"];
+$time = date('Y/m/d H:i:s');
+$captcha = $_POST['g-recaptcha-response'];
+
+if (!$captcha) {
+    echo "Fill out the captcha";
+    exit();
+}
+
+$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey .  '&response=' . $captcha;
+$response = file_get_contents($url);
+$responsekeys = json_decode($response, true);
+if ($responsekeys['success']) {
+    $query = "Insert into messages (name, email, message,time) VALUES ( '$name', '$email', '$message','$time' )";
+    mysqli_query($conn, $query);
+    header("Location: http://hbsmechanical.com");
+} else {
+    header("Location: http://hbsmechanical.com");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// header("Location: http://hbsmechanical.com");
